5ab6b628932648e98ec72f3584da0a403e891a0c
Thomas Maltby <tmaltby19@gmail.com>
2025-05-03 15:41:04 +0100

    create proposal

--- src/app/[lang]/nouns/dreams/[id]/_components/Details/Traits/ProposeLink.tsx
+++ src/app/[lang]/nouns/dreams/[id]/_components/Details/Traits/ProposeLink.tsx
@@ -6,6 +6,7 @@ import { DreamFromDBWithCustomTrait } from '@/utils/dto/Dream/FromDB';
 import Link from 'next/link';
 import { useContext } from 'react';
 import styles from '@/app/[lang]/nouns/dreams/[id]/_styles/details/traits/propose-link.module.css';
+import { useAppKitAccount } from '@reown/appkit/react';
 
 interface Props {
     dict: Dictionary;
@@ -13,9 +14,15 @@ interface Props {
 }
 
 export default function ProposeLink({ dict, dream }: Props) {
+    const { address } = useAppKitAccount();
+
     const { isCandidate } = useContext(ProposalContext);
 
-    if (typeof isCandidate === 'boolean' && !isCandidate) {
+    if (
+        typeof isCandidate === 'boolean' &&
+        !isCandidate &&
+        address === dream.dreamer
+    ) {
         return (
             <Link
                 href={`/nouns/dreams/${dream.id}/propose`}
