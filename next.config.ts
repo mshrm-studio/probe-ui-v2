130396db90cf984603b2967e385a896925225a30
Thomas Maltby <tmaltby19@gmail.com>
2025-12-09 12:43:25 +0700

    revert to webpack

--- next.config.ts
+++ next.config.ts
@@ -21,10 +21,10 @@ const nextConfig: NextConfig = {
             },
         ],
     },
-    // webpack: (config) => {
-    //     config.externals.push('pino-pretty', 'lokijs', 'encoding');
-    //     return config;
-    // },
+    webpack: (config) => {
+        config.externals.push('pino-pretty', 'lokijs', 'encoding');
+        return config;
+    },
 };
 
 export default nextConfig;
